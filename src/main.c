87ee9d9128318caeed42291bfd9ebd6fb64eef82
dmartinge1 <44801981+dmartinge1@users.noreply.github.com>
2018-11-15 14:28:45 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,52 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
 
-#include "my_header.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#define toto =0
+
+int main (void) 
 {
-    printf("hello world");
+
+int s1 = 3 , s2 =3 , s3 = 3 , s4 ,n = 1 , m = 1 , p =1, q=1, s41 = 3, s42 =7;
+  
+  
+while( n< 30 )
+{
+
+s1= s1 + 3*n;
+
+n= n+1;
+
 }
+
+
+
+while ( s2< 9876)
+{
+	
+	s2 = s2 + 3*m;
+
+	m=m+1;
+
+	
+}
+
+while ( p<16)
+{
+	s41 = s41+3*(p+1);
+	p=p+1;
+}
+
+while ( q<6)
+{
+	s42 = s42 +7*(q+1);
+	q=q+1;
+}
+ 
+s4 = s41+s42-63;
+
+
+
+return 0;
+}
+
