32f421c2b5bc1c666b16236cf3e5f0a87e9ae6e1
dmartinge1 <44801981+dmartinge1@users.noreply.github.com>
2018-11-20 17:25:05 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,8 +9,10 @@ int main( )
 	unsigned char bin[10] = {1,5,7,11,45,22,3,4,15,9};
 		 short stat[10] = {-400,999,2,120,45,203,-99,0,15,199};
 	unsigned short dist[10] = {400,999,2,120,45,203,99,0,15,199};
-	
-	int  i , j , k;
+	unsigned char a[5] = { 1,5,9,8,11};
+	unsigned char b[5];
+	unsigned char miroir[6] = { 1,2,3};
+	int  i , j , k, l, m;
 	
 	for(i=0; i<10; i=i+1)
 		
@@ -34,11 +36,18 @@ int main( )
 	  }
 		   for(k=1; k<11; k=k+1)
 		   { dist[k]= dist[k] - dist[k-1];}
+		   
+		   for (l=0;l<4; l=l+1)
+		   { b[l] = a[4-l];}
 	
+		   
+		   for(m=0;m<3;m=m+1)
+	{
+		miroir[m+3]=miroir[2-m];
+	}
 	
 	
 	
 	return 0;
 }
-		
 		
