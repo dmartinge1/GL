d618ea990aed22137928a8865eb40677a030323a
dmartinge1 <44801981+dmartinge1@users.noreply.github.com>
2018-11-27 13:34:06 +0100

    Update exo_58.c

--- exo_58.c
+++ exo_58.c
@@ -1,6 +1,6 @@
 #include "TP_lib_suite.h"
 
-void  initialiserTirage(void);
+
 short tirerNumero(void);
 
 
@@ -8,21 +8,25 @@ int main(void)
 {
 	int suite_j[6] ={4,10,24,30,44,5};
 	
-	int suite_g[6] = {tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()};
+	int suite_g[6] ;
+	int score[100];
 	
-	unsigned char i, j,nb_gagnant=0,D, nb_super_gagnant;
+	unsigned char i, j,nb_gagnant=0,D ;
 	
 		for(D=0;D<100;D++)
+			
 	
 		{	for(i=0;i<6;i++)
 	{ 
 		for (j=0;j<6;j++)
 			
-	{ if(suite_j[i] ==suite_g[j])
+			
+	{   suite_g[j] = tirerNumero();
+		if(suite_j[i] ==suite_g[j])
 		nb_gagnant= nb_gagnant+1;
 	}
 	}
-		}
+	score[D] = nb_gagnant;	}
 	return 0; 
 }
-	
+
