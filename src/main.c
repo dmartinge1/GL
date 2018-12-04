736451250d5be6194019dd9381da3da51ec48892
dmartinge1 <44801981+dmartinge1@users.noreply.github.com>
2018-12-04 18:05:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
-#include "my_header.h"
+#include "ansi_c.h"
 
 int main(void)
 {
-    return 0;
+char prenom  [100];
+char age  [100];
+char nom  [100];
+
+printf ("Prenom:");
+scanf("%s",prenom);
+printf ("Nom:");
+scanf("%s",nom);
+printf ("Age :");
+scanf("%s",age);
+
+printf ("Prenom: %s   Nom:%s   Age:%s ans",prenom ,nom ,age );
+getchar();
+
+return 0;
+
 }
